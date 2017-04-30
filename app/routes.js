440530e009fdf5503d2a0f55b65954d1afc82c40
dadapowell <jasonlalor@gmail.com>
2017-04-30 02:40:30 -0400

    single page

--- app/routes.js
+++ app/routes.js
@@ -10,17 +10,17 @@ module.exports = router;
 
 // route for homepage
 router.get('/', function(req, res) {
-	res.render("pages/index");
+	res.render("pages/en/index");
 });
 
 // route for 1&1's bullshit
 router.get('/defaultsite', function(req, res) {
-	res.render("pages/index");
+	res.render("pages/en/index");
 });
 
 // route for about page
 router.get('/about', function(req, res) {
-	res.render("pages/about");
+	res.render("pages/en/about");
 });
 
 // route for team page
@@ -36,5 +36,49 @@ router.get('/team', function(req, res) {
 
 // route for about page
 router.get('/maps', function(req, res) {
-	res.render("pages/maps");
+	res.render("pages/en/maps");
+});
+
+router.get('/prototypes', function(req, res) {
+	res.render("pages/en/prototype");
+});
+router.get('/prototype', function(req, res) {
+	res.render("pages/en/prototype");
+});
+
+router.get('/en', function(req, res) {
+	res.render("pages/en/index");
+});
+router.get('/en/about', function(req, res) {
+	res.render("pages/en/about");
+});
+router.get('/en/maps', function(req, res) {
+	res.render("pages/en/maps");
+});
+router.get('/en/prototype', function(req, res) {
+	res.render("pages/en/prototype");
+});
+router.get('/es', function(req, res) {
+	res.render("pages/es/index");
+});
+router.get('/es/about', function(req, res) {
+	res.render("pages/es/about");
+});
+router.get('/es/maps', function(req, res) {
+	res.render("pages/es/maps");
+});
+router.get('/es/prototype', function(req, res) {
+	res.render("pages/es/prototype");
+});
+router.get('/zh', function(req, res) {
+	res.render("pages/zh/index");
+});
+router.get('/zh/about', function(req, res) {
+	res.render("pages/zh/about");
+});
+router.get('/zh/maps', function(req, res) {
+	res.render("pages/zh/maps");
+});
+router.get('/zh/prototype', function(req, res) {
+	res.render("pages/zh/prototype");
 });
